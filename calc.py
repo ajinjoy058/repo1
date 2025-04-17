dfe5a2af94f28696a44166be146e592d94476d25
ajinjoy058 <ajinjoy42@gmail.com>
2025-04-17 17:24:39 +0530

    changed spelling

--- calc.py
+++ calc.py
@@ -9,7 +9,7 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Error: Cannot divide by zero"
+        return "Error: Cannot divide by zerooo"
     return x / y
 
 def calculator():
