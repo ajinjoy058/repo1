81de86962be180121d53cc13fed8dac264d78ca2
ajinjoy058 <ajinjoy42@gmail.com>
2025-04-17 15:19:10 +0530

    second change

--- calc.py
+++ calc.py
@@ -18,7 +18,7 @@ def calculator():
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
-    print("4. Dividee")
+    print("4. Divide")
 
     choice = input("Enter choice (1/2/3/4): ")
 
